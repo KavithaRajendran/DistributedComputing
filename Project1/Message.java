84a4df7e1acb33db25c760f5f09000abeb0c8472
Kavitha Rajendran <kavitha.rajendran88@gmail.com>
2018-02-04 01:33:38 -0600

    Update Message.java

--- Project1/Message.java
+++ Project1/Message.java
@@ -1,7 +1,7 @@
 
 /**
- * @author Kavitha Rajendran - kxr161830
- * 		   Rajkumar Panneer selvam - rxp162130 
+ * @author Kavitha Rajendran
+ * 	   Rajkumar Panneer selvam 
  * 
  * Message interface needs to be implemented by all messages need to be exchanged between nodes.
  */
